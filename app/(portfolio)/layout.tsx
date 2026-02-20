b94e95fa12fcb4d6dc705b256b4f85298dcbdd16
rafidoth <rafiulhasan803@gmail.com>
2026-02-20 17:49:37 +0600

    style : fixed responsiveness and other strucure

--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -1,7 +1,6 @@
 import { NavBar } from "@/components/portfolio/nav-bar"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { BGPattern } from "@/components/ui/bg-pattern"
-import Image from "next/image"
+import { WizardGuide } from "@/components/portfolio/wizard-guide"
 
 export default function PortfolioLayout({
     children,
@@ -9,15 +8,15 @@ export default function PortfolioLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="relative overflow-hidden bg-background">
-            <Image src="/wiz.png" alt="wizard" width={100} height={100} className="absolute right-5 bottom-5 rounded-full border-2  border-border" />
-            <BGPattern variant="vertical-lines" mask="fade-bottom" fill="rgba(255,255,255,0.08)" size={16} />
-            <ScrollArea className="flex-1">
-                <main className="mx-auto max-w-5xl px-2 py-12 md:px-4 md:py-16">
-                    <NavBar />
+        <div className="w-full flex justify-center bg-background relative">
+            <WizardGuide />
+            <BGPattern className="pointer-events-none" variant="vertical-lines" mask="fade-bottom" fill="rgba(255,255,255,0.08)" size={16} />
+            <main className="relative z-20 w-full flex flex-col items-center md:max-w-5xl px-2 py-8 md:px-4 md:py-16 gap-6 md:gap-8">
+                <NavBar />
+                <div className="w-full">
                     {children}
-                </main>
-            </ScrollArea>
+                </div>
+            </main>
         </div>
     )
 }
