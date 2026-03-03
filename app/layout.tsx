897809c184f74ce94292c4b446c480a63d209e12
rafidoth <rafiulhasan803@gmail.com>
2026-03-03 13:27:48 +0600

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import { IBM_Plex_Serif } from "next/font/google";
+import { IBM_Plex_Serif, Noto_Sans_Bengali, Tiro_Bangla } from "next/font/google";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export const metadata: Metadata = {
@@ -14,6 +14,18 @@ const ibm = IBM_Plex_Serif({
     weight: ["400", "700", "600", "500"]
 })
 
+const notoBangla = Noto_Sans_Bengali({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["bengali"],
+    variable: "--font-noto-bangla",
+})
+
+const tiroBangla = Tiro_Bangla({
+    weight: ["400"],
+    subsets: ["bengali"],
+    variable: "--font-tiro-bangla",
+})
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -22,7 +34,7 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`antialiased dark ${ibm.className} selection:bg-primary selection:text-black`}>
+                <body className={`antialiased dark ${ibm.className} ${notoBangla.variable} ${tiroBangla.variable} selection:bg-primary selection:text-black`}>
                     {children}
                 </body>
             </html>
